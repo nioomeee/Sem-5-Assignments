2c6832d0b86eaee52518fae7b50f4e5f9ca446be
nioomeee <nioomeee@gmail.com>
2025-08-21 17:55:31 +0530

    area of rect and square

---
+++ Python/Assignment-1/7.py
@@ -0,0 +1,12 @@
+# Write a Python program to calculate the area of rectangle and square.
+
+print("\n1. For rectangle: ")
+length = float(input("Enter length of rectangle: "))
+breadth = float(input("Enter breadth of rectangle: "))
+print(f"The area of rectangle = {length * breadth}")
+print("-" * 20)
+
+print("\n2. For square: ")
+side = float(input("Enter side of a square: "))
+print(f"The area of the square = {side * side}")
+print("-" * 20)
