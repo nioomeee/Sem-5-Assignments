b7c0d0fc18ba9f05f8dfc6758a01cec367a0a8b9
nioomeee <nioomeee@gmail.com>
2025-08-21 17:59:17 +0530

    Python swap

---
+++ Python/Assignment-1/8.py
@@ -0,0 +1,14 @@
+# Write a Python program to swap of two numbers.
+
+# Using special python swap
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print(f"\n Before swap: num1 = {num1}, num2 = {num2}")
+
+num1, num2 = num2, num1
+
+print(f"\n After swap: num1 = {num1}, num2 = {num2}")
+
+
