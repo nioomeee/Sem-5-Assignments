54edc83c368c83f24b9b35b8e99526c6b6b8a79a
nioomeee <nioomeee@gmail.com>
2025-08-21 17:51:56 +0530

    basic range() func

---
+++ Python/Assignment-1/6.py
@@ -0,0 +1,6 @@
+# Write a Python program that will display numbers from 1 to 20(both inclusive) usingrange()function.
+
+print("1 to 20 (both inclusive) using range() function: ")
+
+for i in range(1, 21): 
+    print(i)
