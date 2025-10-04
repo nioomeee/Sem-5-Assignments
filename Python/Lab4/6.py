720ec3754cdbcdf1b18fa638c675d24a32214e4f
nioomeee <nioomeee@gmail.com>
2025-10-05 02:55:40 +0530

    Finally block usage with exception handling

--- Python/Lab4/6.py
+++ Python/Lab4/6.py
@@ -1,3 +1,15 @@
 # Write a program to demonstrate the use of finally with exception.
-# Done before only 
-# Same as 4.py
+
+print(" ----- Number Division -----")
+
+try:
+    numerator = float(input("Enter numerator: "))
+    denominator = float(input("Enter denominator: "))
+    result = numerator / denominator
+    print(f"\n Succes! The reuslt is {result:.2f}")
+
+except ZeroDivisionError:
+    print("\n Error cannot divide by Zero!")
+
+finally:
+    print("\n ----- Program terminated -----")
