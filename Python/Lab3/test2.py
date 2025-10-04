440918cca680903af76ea0b556b4351b6965af28
nioomeee <nioomeee@gmail.com>
2025-10-05 01:22:21 +0530

    2nd program for testing school package

---
+++ Python/Lab3/test2.py
@@ -0,0 +1,8 @@
+from school.students.student_info import student_detail
+from school.teachers.teacher_info import teacher_detail
+
+print(f"----- School Information System -----")
+
+student_detail("Anamika Rai", 15)
+
+teacher_detail("Ms. Vaseeta", "English")
