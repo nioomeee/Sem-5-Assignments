9c1d9d0bae0271ccc5d1f75de5882f4703fbc3ba
nioomeee <nioomeee@gmail.com>
2025-11-19 03:51:12 +0530

    Adding complex numbers

---
+++ Python/All-topics-covered/10.py
@@ -0,0 +1,16 @@
+# Add two complex numbers c1 = 3 + 5j and c2 = 2 + 1j and prints the real part.
+
+c1 = 3 + 5j
+c2 = 2 + 1j
+
+sum_complex = c1 + c2
+
+real_part = sum_complex.real
+
+imag_part = sum_complex.imag
+
+print(f"\n 1st complex number: {c1}")
+print(f"\n 2nd complex number: {c2}")
+print(f"\n Sum of complex numbers: {sum_complex}")
+print(f"\n Real part of the sum: {real_part}")
+print(f"\n Imaginary part of the sum: {imag_part}")
