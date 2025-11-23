85dd613766f2c88354de2436151454d370e0ef4f
Niomi <nioomeee@gmail.com>
2025-11-23 21:26:20 +0530

    final keyword

---
+++ PHP/Exams/task20.php
@@ -0,0 +1,9 @@
+<!-- Parent Class "Department" with method "showDept()". Mark this method as final.
+
+Child Class "CSE" extends "Department".
+
+Fail Test: Try to override "showDept()" in the child class and 
+see the error (comment it out to fix).
+
+Display: Create a "CSE" object and call the parent method. -->
+
